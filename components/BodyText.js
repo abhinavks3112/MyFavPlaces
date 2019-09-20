194dc3d7414008bfa04b369cf508046d84befd6b
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-20 18:59:34 +0530

    Wrapper for Text comp for use in body and title

---
+++ components/BodyText.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const BodyText = (props) => {
+    const { children, style } = props;
+    return <Text style={{ ...styles.text, ...style }}>{children}</Text>;
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontFamily: 'amita',
+        fontSize: 16
+    }
+});
+
+export default BodyText;
