dbefe79211aa1d9eebac3006301215cd50faf25f
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-20 19:13:02 +0530

    Added a model class to describe a place object

---
+++ models/Place.js
@@ -0,0 +1,11 @@
+class Place {
+    constructor(id, name, location, image, description) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.image = image;
+        this.description = description;
+    }
+}
+
+export default Place;
