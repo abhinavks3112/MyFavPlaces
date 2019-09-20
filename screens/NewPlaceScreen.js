76013f7a5345b04984a0a2b3fd58f66193c18589
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-20 19:20:47 +0530

    Added basic form for adding a new place

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+ ScrollView, View, TextInput, StyleSheet
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-const NewPlaceScreen = () => {
+import TitleText from '../components/TitleText';
+import CustomHeaderButton from '../components/CustomHeaderButton';
+
+import { addPlace } from '../store/actions/placesAction';
+
+const NewPlaceScreen = (props) => {
+    const { navigation } = props;
+    const [titleValue, setTitleValue] = useState('');
+    const dispatch = useDispatch();
+
+    const saveButtonHandler = useCallback(() => {
+        dispatch(addPlace(titleValue, '', '', ''));
+        navigation.navigate('Places');
+    }, [dispatch, titleValue]);
+
+    useEffect(() => {
+        navigation.setParams({ save: saveButtonHandler });
+    }, [saveButtonHandler]);
+
     return (
-    <View style={styles.screen}>
-        <Text>New Place Screen</Text>
-    </View>
+        <ScrollView style={styles.screen}>
+            <View style={styles.form}>
+                <TitleText style={styles.label}>Title</TitleText>
+                <TextInput
+                    style={styles.input}
+                    value={titleValue}
+                    onChangeText={(text) => setTitleValue(text)}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
-NewPlaceScreen.navigationOptions = () => {
+NewPlaceScreen.navigationOptions = (navData) => {
+    const { navigation } = navData;
+    const save = navigation.getParam('save');
+
     return {
-        headerTitle: 'New Place'
+        headerTitle: 'New Place',
+        headerRight: (
+        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item
+            iconName="save"
+            title="Save Place"
+            onPress={() => save()}
+            />
+        </HeaderButtons>
+        )
     };
 };
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        flex: 1
+    },
+    form: {
+        margin: 30
+    },
+    label: {
+        marginBottom: 15
+    },
+    input: {
+        borderBottomColor: '#777',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
     }
 });
 
