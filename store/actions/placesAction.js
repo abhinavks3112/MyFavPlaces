9091c4da52aa5e3cb046f59154d2553be5d7c4df
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-22 22:16:08 +0530

    Code for moving the image to a permanent location on device

--- store/actions/placesAction.js
+++ store/actions/placesAction.js
@@ -1,13 +1,26 @@
+import * as FileSystem from 'expo-file-system';
 import { ADD_PLACE } from './types';
 
-export const addPlace = (name, location, image, description) => ({
-    type: ADD_PLACE,
-    place: {
-        name,
-        location,
-        image,
-        description
+export const addPlace = (name, location, image, description) => async (dispatch) => {
+    /* Split the image path into array of segments and take the last segment eg
+    myFolder/myImage.jpeg => ['myFolder', 'myImage.jpeg'] */
+    const fileName = image.split('/').pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+    try {
+        // Moving the iamge to a permanent location on device
+        await FileSystem.moveAsync({ from: image, to: newPath });
+    } catch (error) {
+        throw new Error('Something unexpected happened when moving the file');
     }
-});
+    dispatch({
+        type: ADD_PLACE,
+        place: {
+            name,
+            location,
+            image: newPath,
+            description
+        }
+    });
+};
 
 export const updatePlace = () => ({});
