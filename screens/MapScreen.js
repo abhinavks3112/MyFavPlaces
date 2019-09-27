7a5f52c1c07fbf7b4e726c9419af72c549cd5841
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-27 19:40:37 +0530

    Save & pass marked location, initial mark location if locate me option chosen in previous screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,32 +1,65 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, Alert } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const MapScreen = (props) => {
     const { navigation } = props;
     const [selectedLocation, setSelectedLocation] = useState();
     const location = navigation.getParam('location');
+    const latitudeDelta = 0.0922;
+    const longitudeDelta = 0.0421;
+    let markerCoordinate;
+
     let mapRegion = null;
+
     if (!location) {
     // default region
         mapRegion = {
         latitude: 37.5,
         longitude: -122.43,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
+        latitudeDelta,
+        longitudeDelta
     };
     } else {
-        mapRegion = location;
+        mapRegion = {
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta,
+            longitudeDelta
+        };
+        markerCoordinate = location;
     }
 
     const selectLocationHandler = (event) => {
         setSelectedLocation(event.nativeEvent.coordinate);
     };
 
-    let markerCoordinate;
+    const saveMarkedLocationHandler = useCallback(() => {
+        if (!selectedLocation) {
+            Alert.alert('No Location Chosen', 'Please choose a location and then save!!', [{ text: 'Okay' }]);
+            return;
+        }
+        navigation.navigate('NewPlace', {
+            pickedLocation: selectedLocation
+        });
+    }, [selectedLocation]);
 
-    if(selectedLocation) {
-        markerCoordinate = selectedLocation
+    useEffect(() => {
+        navigation.setParams({
+            save: saveMarkedLocationHandler
+        });
+    }, [saveMarkedLocationHandler]);
+
+    if (selectedLocation) {
+        markerCoordinate = selectedLocation;
+        mapRegion = {
+            latitude: selectedLocation.latitude,
+            longitude: selectedLocation.longitude,
+            latitudeDelta,
+            longitudeDelta
+        };
     }
 
     return (
@@ -37,17 +70,28 @@ const MapScreen = (props) => {
     >
        {markerCoordinate && (
            <Marker
-            title='Picked Location'
+            title="Picked Location"
             coordinate={markerCoordinate}
-            ></Marker>
+           />
         )}
     </MapView>
     );
 };
 
-MapScreen.navigationOptions = () => ({
-        headerTitle: 'Map'
-});
+MapScreen.navigationOptions = (navData) => {
+    const { navigation } = navData;
+    const save = navigation.getParam('save');
+    return ({
+        headerTitle: 'Map',
+        headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                        <Item
+                        iconName="save"
+                        title="Save Marker"
+                        onPress={save}
+                        />
+                     </HeaderButtons>
+    });
+};
 
 const styles = StyleSheet.create({
     map: {
