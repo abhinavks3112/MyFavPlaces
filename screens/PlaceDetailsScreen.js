1cd01bb3b3e4c1f85da9469164a5008d383ea233
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-09-28 19:50:47 +0530

    Place deatils screen populated with place details

--- screens/PlaceDetailsScreen.js
+++ screens/PlaceDetailsScreen.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+ ScrollView, View, StyleSheet, Image
+} from 'react-native';
+import { useSelector } from 'react-redux';
 
-const PlaceDetailsScreen = () => {
+import MapPreview from '../components/MapPreview';
+import BodyText from '../components/BodyText';
+import { convertDMS } from '../components/LatLongToDMS';
+
+const PlaceDetailsScreen = (props) => {
+    const { navigation } = props;
+    const placeId = navigation.getParam('placeId');
+    const selectedPlace = useSelector(
+        (state) => state.places.places.find((place) => place.id === placeId)
+    );
+
+    const formattedCoords = convertDMS(
+        selectedPlace.location.latitude, selectedPlace.location.longitude
+    );
+
     return (
-    <View style={styles.screen}>
-        <Text>Places Details Screen</Text>
-    </View>
+    <ScrollView style={{ flex: 1 }} contentContainerStyle={styles.screen}>
+        <View style={styles.imageContainer}>
+            <Image source={{ uri: selectedPlace.image }} style={styles.image} />
+        </View>
+        <View style={styles.contentContainer}>
+            <View style={styles.locationContainer}>
+                <BodyText style={styles.location}>{formattedCoords}</BodyText>
+            </View>
+            <View style={styles.mapPreview}>
+                <MapPreview location={selectedPlace.location} scrollEnabled={false} />
+            </View>
+        </View>
+    </ScrollView>
     );
 };
 
@@ -19,9 +46,47 @@ PlaceDetailsScreen.navigationOptions = (navData) => {
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        height: 300,
+        width: '100%',
+        marginBottom: 10
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    contentContainer: {
+        width: '90%',
+
+        shadowColor: '#ccc',
+        shadowOffset: {
+            height: 1,
+            width: 0
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+        elevation: 1,
+
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10,
+        margin: 10,
+        overflow: 'hidden'
+    },
+    locationContainer: {
+        alignItems: 'center'
+    },
+    location: {
+        color: 'grey'
+    },
+    mapPreview: {
+        height: 300,
+        width: '100%',
+        borderColor: '#ccc',
+        borderWidth: 1
     }
 });
 
